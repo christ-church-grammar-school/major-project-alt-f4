375dfaea6992e52706001c2ec597c894c516909b
Cameron Minchin <1022765@student.ccgs.wa.edu.au>
2019-09-03 14:56:05 +0800

    cards magnify

--- 1000 Blank White Cards/GameUI.xaml.cs
+++ 1000 Blank White Cards/GameUI.xaml.cs
@@ -13,6 +13,7 @@ namespace _1000_Blank_White_Cards
     {
         public EventHandler ladder;
         List<Button> buttons = new List<Button>();
+        int globalCounter = 1;
 
         public GameUI()
         {
@@ -43,15 +44,33 @@ namespace _1000_Blank_White_Cards
             buttons[buttons.Count - 1].Click += playCard;
             buttons[buttons.Count - 1].Height = 77;
             buttons[buttons.Count - 1].Width = 59;
-
-            buttons[buttons.Count - 1].MouseEnter += new { 
-                Console.WriteLine("ligma ballz");
-                DrawCard.Margin = new Thickness(563, 50, 31, 230);
-            };
-            
+            buttons[buttons.Count - 1].Name = "Button"+ Convert.ToString(globalCounter);
+            globalCounter += 1;
+            buttons[buttons.Count - 1].MouseEnter += bigg;
+            buttons[buttons.Count - 1].MouseLeave += smol;
             reorganiseCards();
         }
         
+        private void bigg(object sender, RoutedEventArgs e)
+        {
+            Button card = (Button)sender;
+            card.Margin = new Thickness(card.Margin.Left - 10, card.Margin.Top - 15, 0, 0);
+            card.Height += 30;
+            card.Width += 20;
+            reorganiseCards();
+            GameUIGrid.Children.RemoveAt((int)GameUIGrid.Children.IndexOf(card));
+            GameUIGrid.Children.Add(card);
+        }
+
+        private void smol(object sender, RoutedEventArgs e)
+        {
+            Button card = (Button)sender;
+            card.Margin = new Thickness(card.Margin.Left + 10, card.Margin.Top + 15, 0, 0);
+            card.Height -= 30;
+            card.Width -= 20;
+            reorganiseCards();
+        }
+
         private void reorganiseCards()
         {
             if (buttons.Count > 9)
@@ -59,6 +78,8 @@ namespace _1000_Blank_White_Cards
                 for (var x = 0; x < buttons.Count; x++)
                 {
                     buttons[x].Margin = new Thickness((x+1) * 120 * 9 / buttons.Count - 590, 270,0 , 0);
+                    GameUIGrid.Children.RemoveAt((int)GameUIGrid.Children.IndexOf(buttons[x]));
+                    GameUIGrid.Children.Add(buttons[x]);
                 }
             }
             else
@@ -66,6 +87,8 @@ namespace _1000_Blank_White_Cards
                 for (var x = 0; x < buttons.Count; x++)
                 {
                     buttons[x].Margin = new Thickness((x+1) * 120 - 590, 270, 0, 0);
+                    GameUIGrid.Children.RemoveAt((int)GameUIGrid.Children.IndexOf(buttons[x]));
+                    GameUIGrid.Children.Add(buttons[x]);
                 }
             }
         }
@@ -77,13 +100,11 @@ namespace _1000_Blank_White_Cards
 
         private void DrawCard_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            Console.WriteLine("ligma ballz");
             DrawCard.Margin = new Thickness(563, 50, 31, 230);
         }
 
         private void DrawCard_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            Console.WriteLine("unligma ballz");
             DrawCard.Margin = new Thickness(568, 55, 36, 235);
         }
     }
