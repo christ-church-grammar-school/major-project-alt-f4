653075393bd9b488434356c0fe3b92b3a50a977e
Cameron Minchin <1022765@student.ccgs.wa.edu.au>
2019-10-30 09:01:16 +0800

    added a bunch of usings to allow implimentation of connectivity and deleted random redundent space

--- 1000 Blank White Cards/GameUI.xaml.cs
+++ 1000 Blank White Cards/GameUI.xaml.cs
@@ -7,6 +7,20 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Windows.Input;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace _1000_Blank_White_Cards
 {
@@ -212,7 +226,6 @@ namespace _1000_Blank_White_Cards
             DrawCard.Height -= 10;
             DrawCard.Width -= 8;
         }
-
 
         private void GimmeText(object sender, RoutedEventArgs e)
         {
